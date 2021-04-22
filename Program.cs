5f527e5cf6db079c87011e58c366330db9a9ca51
PhilippeRasoa <prasoa@citypassenger.com>
2021-04-22 16:31:33 +0200

    arch release file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wpf_Detect_DHCP_kavi_ok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
